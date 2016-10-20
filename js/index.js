8fba7780185c1b81d4cc146a76a4bfd70e30d5b0
Jon Schuster <graphics@nopp.com>
2016-10-20 19:40:38 -0400

    just playing around

--- js/index.js
+++ js/index.js
@@ -5,72 +5,17 @@ $('p').each(function() {
 
 
 
-// Lazy load backgrounds
-// function lazyLoadBg(el, imageSource) {
-//   $('<img/>').attr('src', imageSource).on({
-//     load: function() {
-//       $(this).remove();
-//       $(el).css('backgroundImage', 'url(' + imageSource + ')');
-//       $(el).css('opacity', '1');
-//     },
-//     error: function() {
-//       $(this).remove();
-//     }
-//   });
-// }
-//
-// $('.lazy').each(function(index, el) {
-//   lazyLoadBg(el, $(el).data('src'));
-// });
-
-
-
-// Simple parallax background
-// var cover = document.querySelector('.parallax'),
-//   coverHeight = Math.round(cover.offsetHeight),
-//   translate,
-//   parallaxThreshold = 3;
-//
-// function parallax() {
-//   if (window.scrollY < coverHeight) {
-//     translate = Math.round(window.scrollY / parallaxThreshold);
-//     window.requestAnimationFrame(function() {
-//       cover.style.transform = 'translateY(' + translate + 'px)';
-//     });
-//   }
-// }
-//
-// parallax();
-//
-// window.addEventListener('scroll', parallax, false);
-//
-// window.addEventListener('resize', debounce(function() {
-//   coverHeight = Math.round(cover.offsetHeight);
-// }, 800));
-//
-// function debounce(fn, wait) {
-//   var timeout;
-//   return function() {
-//     clearTimeout(timeout);
-//     timeout = setTimeout(function() {
-//       fn.apply(this, arguments)
-//     }, (wait || 1));
-//   }
-// }
-
-
-
 // Simple slider
-// $(document).ready(function(){
-//   $('.slider').slick({
-//     draggable: true,
-//     arrows: true,
-//     dots: true,
-//     fade: false,
-//     speed: 900,
-//     infinite: true,
-//     autoplay: true,
-//     slidesPerView: 1,
-//     touchThreshold: 100
-//   });
-// });
+$(document).ready(function(){
+  $('.slider').slick({
+    draggable: true,
+    arrows: true,
+    dots: true,
+    fade: false,
+    speed: 900,
+    infinite: true,
+    autoplay: true,
+    slidesPerView: 1,
+    touchThreshold: 100
+  });
+});
