10b62dc01ab03fe7831b277872da0efeb1fd47af
Jon Schuster <aonmedia@live.com>
2016-11-16 14:07:31 -0500

    add parallax header

--- js/index.js
+++ js/index.js
@@ -26,3 +26,38 @@ $(document).ready(function(){
     touchThreshold: 100
   });
 });
+
+
+
+// Simple parallax background
+var cover = document.querySelector('.parallax'),
+  coverHeight = Math.round(cover.offsetHeight),
+  translate,
+  parallaxThreshold = 3;
+
+function parallax() {
+  if (window.scrollY < coverHeight) {
+    translate = Math.round(window.scrollY / parallaxThreshold);
+    window.requestAnimationFrame(function() {
+      cover.style.transform = 'translateY(' + translate + 'px)';
+    });
+  }
+}
+
+parallax();
+
+window.addEventListener('scroll', parallax, false);
+
+window.addEventListener('resize', debounce(function() {
+  coverHeight = Math.round(cover.offsetHeight);
+}, 800));
+
+function debounce(fn, wait) {
+  var timeout;
+  return function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      fn.apply(this, arguments)
+    }, (wait || 1));
+  }
+}
