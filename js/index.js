dee8d738ea4b3a5d39f95b84812b7ffcf7d0be07
Jon Schuster <aonmedia@live.com>
2017-03-03 09:31:54 -0500

    updated parallax effect
    a few other things here and there, but now the JS file is trimmed WAY
    down, and parallax works on more than one element. Still need to sort
    out the origin image positioning on smaller devices.

--- js/index.js
+++ js/index.js
@@ -6,34 +6,6 @@ $('p').each(function() {
 
 
 // Simple parallax background
-var cover = document.querySelector('.parallax'),
-  coverHeight = Math.round(cover.offsetHeight),
-  translate,
-  parallaxThreshold = 3;
-
-function parallax() {
-  if (window.scrollY < coverHeight) {
-    translate = Math.round(window.scrollY / parallaxThreshold);
-    window.requestAnimationFrame(function() {
-      cover.style.transform = 'translateY(' + translate + 'px)';
-    });
-  }
-}
-
-parallax();
-
-window.addEventListener('scroll', parallax, false);
-
-window.addEventListener('resize', debounce(function() {
-  coverHeight = Math.round(cover.offsetHeight);
-}, 800));
-
-function debounce(fn, wait) {
-  var timeout;
-  return function() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function() {
-      fn.apply(this, arguments)
-    }, (wait || 1));
-  }
-}
+$(window).scroll(function(){
+  $('.parallax').css('background-position','center calc(50% + '+($(window).scrollTop()*0.25)+'px');
+});
