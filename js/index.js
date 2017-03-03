d847a44c476a674c71024bee6853c316ca762e75
Jon Schuster <aonmedia@live.com>
2017-03-03 11:00:44 -0500

    added revealing footer
    fixed some other minor issues, mainly with the parallax covers and the
    footer itself. Still need to sort the origin image position for the
    parallax covers; the header looks fine, regardless of device size, but
    further down the page it's broken.

--- js/index.js
+++ js/index.js
@@ -6,6 +6,47 @@ $('p').each(function() {
 
 
 // Simple parallax background
-$(window).scroll(function(){
-  $('.parallax').css('background-position','center calc(50% + '+($(window).scrollTop()*0.25)+'px');
+function Parallax() {
+	scrollPos = $(this).scrollTop();
+	$('.parallax').css({
+		'background-position' : '50% ' + (-scrollPos/4)+"px"
+	});
+	$('.parallax-content').css({
+		'margin-top': (scrollPos/4)+"px",
+		'opacity': 1-(scrollPos/250)
+	});
+}
+$(document).ready(function(){
+	$(window).scroll(function() {
+		Parallax();
+	});
 });
+
+
+
+// Footer reveal
+$(document).ready(function() {
+  var footerHeight = $('footer').height();
+
+  $('.content').css({
+    'margin-bottom': footerHeight + 'px'
+  });
+
+  scrollFooter(window.pageYOffset, footerHeight);
+
+  window.onscroll = function() {
+    scrollFooter(window.pageYOffset, footerHeight);
+  }
+});
+
+function scrollFooter(scrollY, heightFooter) {
+  if (scrollY >= heightFooter) {
+    $('footer').css({
+      'bottom': '0px'
+    });
+  } else {
+    $('footer').css({
+      'bottom': '-' + heightFooter + 'px'
+    });
+  }
+}
