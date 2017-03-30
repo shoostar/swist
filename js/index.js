8f147eaa743bf0a11c47776e4cd1db6d67266582
Jon Schuster <aonmedia@live.com>
2017-03-30 10:09:54 -0400

    remove .parallax class on mobile detection

--- js/index.js
+++ js/index.js
@@ -5,25 +5,6 @@ $('p').each(function() {
 
 
 
-// Simple parallax background
-function Parallax() {
-	scrollPos = $(this).scrollTop();
-	$('.parallax').css({
-		'background-position' : '50% ' + (50 + scrollPos/50)+"%"
-	});
-	$('.parallax-content').css({
-		'margin-top': (scrollPos/1.5)+"px",
-		'opacity': 1-(scrollPos/500)
-	});
-}
-$(document).ready(function(){
-	$(window).scroll(function() {
-		Parallax();
-	});
-});
-
-
-
 // Footer reveal
 $(document).ready(function() {
   var footerHeight = $('footer').height();
@@ -42,3 +23,28 @@ $(document).ready(function() {
 function scrollFooter(scrollY, heightFooter) { if (scrollY >= heightFooter) {
 $('footer').css({ 'bottom': '0px' }); } else { $('footer').css({ 'bottom': '-' +
 heightFooter + 'px' }); } }
+
+
+
+// Simple parallax background
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  $('header').removeClass('.parallax');
+  $('section').removeClass('.parallax');
+} else
+{
+  function Parallax() {
+  	scrollPos = $(this).scrollTop();
+  	$('.parallax').css({
+  		'background-position' : '50% ' + (50 + scrollPos/50)+"%"
+  	});
+  	$('.parallax-content').css({
+  		'margin-top': (scrollPos/1.5)+"px",
+  		'opacity': 1-(scrollPos/250)
+  	});
+  }
+  $(document).ready(function(){
+  	$(window).scroll(function() {
+  		Parallax();
+  	});
+  });
+}
